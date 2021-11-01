a1b12bf05cd98555c858ebcb3eb5d683710423cd
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2021-11-01 19:30:27 +0700

    File notasi_sigma1.py, notasi_sigma2.py dan notasi_pi.py digabungkan ke dalam file tugas 5.

---
+++ 0110221095_Muhammad Amar Dafi_Latihan baris dan deret.py
@@ -0,0 +1,24 @@
+# notasi sigma 1
+n = 17
+i = 13
+hasil = 0
+for x in range (i , n+1):
+    hasil = hasil + 5 * i + 2
+
+print(hasil)
+
+# notasi sigma 2
+nilai_akhir = 10
+n = 5
+hasil = 0
+for x in range (n, nilai_akhir+1):
+    hasil = hasil + ((x ** 2) + (2 * x) -1)
+print(hasil)
+
+# notasi pi
+n = 8
+i = 4
+hasil = 1
+for x in range(i, n+1):
+    hasil = hasil * (x-3)
+print(hasil) 
